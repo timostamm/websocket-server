7881ee2be32fb858a7d4d9ef1c7b3f294a22b534
Timo Stamm <ts@timostamm.de>
2018-09-11 13:19:47 +0200

    cleanup

--- autobahn-test/fuzzing/server.php
+++ autobahn-test/fuzzing/server.php
@@ -1,42 +1,41 @@
 <?php
 
-use TS\Websockets\WebSocketServer;
+use React\EventLoop\LoopInterface;
 use TS\Websockets\ControllerInterface;
 use TS\Websockets\WebSocket;
-use Ratchet\RFC6455\Messaging\MessageInterface;
+use TS\Websockets\WebSocketServer;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 
-$port = $argc > 1 ? $argv[1] : 8000;
-$impl = sprintf('React\EventLoop\%s', $argc > 2 ? $argv[2] : 'StreamSelectLoop');
-$loop = new $impl;
+/** @var LoopInterface $loop */
+$uri = sprintf('127.0.0.1:%s', $argv[1] ?? 8000);
+$loopClass = sprintf('React\EventLoop\%s', $argv[2] ?? 'StreamSelectLoop');
+$loop = new $loopClass;
+$server = new WebSocketServer($loop, ['uri' => $uri]);
 
-$server = new WebSocketServer($loop, [
-    'uri' => '127.0.0.1:' . $port
-]);
+print '[server.php] Websocket server using ' . $loopClass . ' listening on ' . $server->getAddress() . PHP_EOL;
 
-print 'Websocket server using ' . $impl . ' listening on ' . $server->getAddress() . PHP_EOL;
 
-$server->addRoute('*', new class implements ControllerInterface {
+$server->addRoute('*', new class implements ControllerInterface
+{
     function onMessage(WebSocket $from, string $payload, bool $binary): void
     {
         $from->send($payload, $binary);
     }
-    function onOpen(WebSocket $connection): void {}
-    function onClose(WebSocket $connection): void {}
-    function onError(WebSocket $connection, \Throwable $error): void {}
+
+    function onOpen(WebSocket $connection): void
+    {
+    }
+
+    function onClose(WebSocket $connection): void
+    {
+    }
+
+    function onError(WebSocket $connection, \Throwable $error): void
+    {
+    }
 });
 
 
-// This is enabled to test https://github.com/ratchetphp/Ratchet/issues/430
-// The time is left at 10 minutes so that it will not try to every ping anything
-// This causes the Ratchet server to crash on test 2.7
-//$wsServer->enableKeepAlive($loop, 600);
-
-//$app = new Ratchet\Http\HttpServer($wsServer);
-
-//$server = new Ratchet\Server\IoServer($app, $sock, $loop);
-
 $loop->run();
-
